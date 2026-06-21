f8a28107b86308d72f9613f24a6ab05662d99cc6
krish <ambwanikrish127@gmail.com>
2026-06-22 03:09:32 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-col bg-gray-900 text-center text-white'>
+      <Header />
+      <main className='grow'>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  ) : null
+}
+
+export default App
